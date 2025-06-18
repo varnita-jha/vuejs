0ca0ed83b4b411281c60d49307ccafc2c18bf5cd
varnita-jha <39858796+varnita-jha@users.noreply.github.com>
2025-06-18 12:55:26 +0530

    Text Interpolation

--- composition-api-projects-and-concepts/src/App.vue
+++ composition-api-projects-and-concepts/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import MyComponent from './components/MyComponent.vue';
 </script>
 
 <template>
-  <HelloWorld/>
+  <MyComponent/>
 </template>
   
 <style scoped></style>
--- composition-api-projects-and-concepts/src/components/MyComponent.vue
+++ composition-api-projects-and-concepts/src/components/MyComponent.vue
@@ -1,18 +1,16 @@
-<template>
-    <h1>This is a parent Component</h1>
-    <AnotherComponent/>
-</template>
-
-<!-- CSS -->
-<style>
-    /* h1{
-        color: red;
-    } */
-</style>
-
-<!-- JS -->
 <script setup>
-    // console.log('Hello World')
-    import AnotherComponent from './AnotherComponent.vue';
-    
+const myMessage = 'Yellow World!';
+const myNumber = 100;
+function add(x, y) {
+    return x + y
+}
 </script>
+
+<template>
+    <!-- Text Interpolation (shows dynamic texts) -->
+    <h1>This is {{ myMessage }}</h1>
+    <h2>My Number: {{ myNumber }}</h2>
+    <h3>2 + 2 = {{ 2 + 2 }}</h3>
+    <h1>Add two numbers (2 + 2): {{ add(2, 2) }}</h1>
+    <!-- Note: we can't declare a variable or use if statement in Text Interpolation -->
+</template>
