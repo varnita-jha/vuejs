6a77ab4d222b4ece75a95978fe58b724b64da23f
varnita-jha <39858796+varnita-jha@users.noreply.github.com>
2025-07-09 13:18:38 +0530

    Attribute Binding updates: image not loading, issue resolved

--- composition-api-projects-and-concepts/src/components/AttributeBinding.vue
+++ composition-api-projects-and-concepts/src/components/AttributeBinding.vue
@@ -1,6 +1,7 @@
 <script setup>
 // const myChannel = "https://youtu.be/pgWZLS75Nmo?si=JRNHQ7aHxSC08kYN";
-const campus = 'C:/Users/IN0VMJH/Downloads/College Campus.jpg'
+const campus = '/images/College Campus.jpg'
+const alternate="campus photo"
 const imageWidth = 400;
 const imageHeight = 400;
 </script>
@@ -9,5 +10,5 @@ const imageHeight = 400;
     <!-- <a v-bind:href = "myChannel">VueJs Tutorial with Projects</a> -->
     <!-- <a :href="myChannel">VueJs Tutorial with Projects</a> -->
     <!-- the above 2 examples give same output (so "v-bind" keyword in not necessary for this (just ":" works)) -->
-    <img :src="campus" alt="campus photo" :width="imageWidth" :height="imageHeight"/>
+    <img :src="campus" :alt="alternate" :width="imageWidth" :height="imageHeight"/>
 </template>
